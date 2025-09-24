d2a9b21c2210d79d67d3e11a4f83ba2b6a2c753c
oisin-t-ucd <oisin.tohak.ucd@gmail.com>
2025-09-24 06:19:13 +0100

    update

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -1,18 +1,16 @@
 import { Server, Model } from "miragejs";
+import jsonData from "../data/posts.json";
 
-export async function makeServer(initialData = {}) {
+export async function makeServer() {
 
-    // let res = await fetch('/newdeploy/posts.json') // here 'newdeploy' should be replaced with the name of your GitHub repository which is also the astro 'base' in astro.config.mjs
-    // let data = await res.json()
-    console.log(initialData)
     let server = new Server({
         environment: "development",
         models: { post: Model },
         seeds(server) {
-            server.db.loadData({ posts: initialData });
+            server.db.loadData({ posts: jsonData });
         },
         routes() {
-            this.namespace = 'api'; // remember to include this when making requests
+            this.namespace = 'api'; // remember to include this when making requests, for example the URL will be '/api/posts/'
             this.get('/posts');
             this.get('/posts/:id');
             this.post('/posts');
